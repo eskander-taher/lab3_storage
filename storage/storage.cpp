88f1be66344cea6d69d6dcdbb5ae3fcc2e83a1a0
eskander-taher <eskander.alshaibany@gmail.com>
2021-12-11 04:09:13 +0500

    add Ancestor class

---
+++ storage/storage.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstring>
+#include <ctime>
+
+using namespace std;
+//functions for styling
+void  highlight(string s);
+void  yellow(string s);
+void  grean(string s);
+#include <iostream>
+
+
+class Ancestor
+{
+public:
+    virtual ~Ancestor()
+    {
+        grean("~Ancestor()");
+    }
+
+    virtual void draw() = 0;
+
+    virtual void area(int x, int y) = 0;
+};
+
+
+int main()
+{
+    return 0;
+}
+
